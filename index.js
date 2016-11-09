12bdac6a2d647197a76823d51a1a6a1263e7a4a3
Michael Kortstiege <mk@nodexo.de>
2016-11-09 19:30:39 +0100

    fix checking for infinity of numbers

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function depictsWholeNumber (v) {
   if (typeof v === 'string') {
     return v === '0' || !!/^[1-9][0-9]*$/.exec(v)
   } else if (typeof v === 'number') {
-    if (('' + v).indexOf('e') > -1) {
+    if (!isFinite(v) || ('' + v).indexOf('e') > -1) {
       return false
     }
     return v === Math.abs(Math.round(v))
